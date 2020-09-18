baefad6ae4d6cd440b689d2a4a39320bcc54fc60
José Cordero Bazurto <jose.cordero2016@uteq.edu.ec>
2020-09-18 00:01:30 -0500

    Initial commit

---
+++ app/src/main/java/com/e/revistasuteq/MainActivity.java
@@ -0,0 +1,50 @@
+package com.e.revistasuteq;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rclRevistas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+
+        rclRevistas = (RecyclerView) findViewById(R.id.rclRevistas);
+        LinearLayoutManager linear = new LinearLayoutManager(getApplicationContext());
+        linear.setOrientation(LinearLayoutManager.HORIZONTAL);
+        rclRevistas.setLayoutManager(linear);
+
+       
+
+    }
+}
